7e96b1889dd698fb30c97219b11f2fffb5aa5183
Nicolas Fournier <nicfournier7@gmail.com>
2022-12-11 01:16:17 -0500

    Tabs into spaces

--- day01/day01.go
+++ day01/day01.go
@@ -14,42 +14,42 @@ import (
 )
 
 func main() {
-	fmt.Println("Advent Of Code 2022: Day01")
+    fmt.Println("Advent Of Code 2022: Day01")
 
-	start := time.Now()
+    start := time.Now()
 
-	readFile, err := os.Open("day01/data/food.elves")
+    readFile, err := os.Open("day01/data/food.elves")
 
-	if err != nil {
-		fmt.Println(err)
-	}
-	fileScanner := bufio.NewScanner(readFile)
+    if err != nil {
+        fmt.Println(err)
+    }
+    fileScanner := bufio.NewScanner(readFile)
 
-	fileScanner.Split(bufio.ScanLines)
+    fileScanner.Split(bufio.ScanLines)
 
-	var maxCaloriesCarried int64 = 0
-	var sumColoriesCarried int64 = 0
+    var maxCaloriesCarried int64 = 0
+    var sumColoriesCarried int64 = 0
 
-	for fileScanner.Scan() {
-		fileLine := fileScanner.Text()
+    for fileScanner.Scan() {
+        fileLine := fileScanner.Text()
 
-		if len(fileLine) > 0 {
-			calories, err := strconv.ParseInt(fileLine, 10, 32)
-			if err == nil {
-				sumColoriesCarried += calories
-			}
-		} else {
-			if sumColoriesCarried > maxCaloriesCarried {
-				maxCaloriesCarried = sumColoriesCarried
-			}
-			sumColoriesCarried = 0
-		}
-	}
-	readFile.Close()
+        if len(fileLine) > 0 {
+            calories, err := strconv.ParseInt(fileLine, 10, 32)
+            if err == nil {
+                sumColoriesCarried += calories
+        }
+        } else {
+            if sumColoriesCarried > maxCaloriesCarried {
+                maxCaloriesCarried = sumColoriesCarried
+            }
+            sumColoriesCarried = 0
+        }
+    }
+    readFile.Close()
 
-	end := time.Now()
-	elapsed := end.Sub(start)
+    end := time.Now()
+    elapsed := end.Sub(start)
 
-	fmt.Println(fmt.Sprintf("Max calories carried: %d", maxCaloriesCarried))
-	fmt.Printf("Time elapsed: %d ns", elapsed.Nanoseconds())
+    fmt.Println(fmt.Sprintf("Max calories carried: %d", maxCaloriesCarried))
+    fmt.Printf("Time elapsed: %d ns", elapsed.Nanoseconds())
 }
